ba4949a8e8412a5afadeeadb7886846983c3223b
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-09-22 13:21:20 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div class="navbar bg-base-100 shadow-sm">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
